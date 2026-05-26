8fe62514838d4ead7cab2ae40188b8ff64b38f7b
Chase Rogness <chaserogness@gmail.com>
2026-05-26 12:30:52 -0500

    feat: Blockbreak events for modded logs

--- src/main/java/com/finestgit/adventurecraft/lumberjack/gameplay/woodcutting/WoodcuttingEvents.java
+++ src/main/java/com/finestgit/adventurecraft/lumberjack/gameplay/woodcutting/WoodcuttingEvents.java
@@ -1,7 +1,10 @@
 package com.finestgit.adventurecraft.lumberjack.gameplay.woodcutting;
 
 import com.finestgit.adventurecraft.lumberjack.AdventureCraftLumberjack;
+import com.finestgit.adventurecraft.lumberjack.progression.LumberjackAttachments;
+import com.finestgit.adventurecraft.lumberjack.registry.LumberjackBlocks;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
@@ -10,5 +13,22 @@ import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 public class WoodcuttingEvents {
     @SubscribeEvent
     public static void onBlockBreakEvent(BreakBlockEvent event) {
+        // If the event is a client side event, ignore it.
+        if (event.getLevel().isClientSide()) {
+            return;
+        }
+        // If the event has been cancelled somehow, ignore it.
+        if (event.isCanceled()) {
+            return;
+        }
+        // If the player is not a server player, ignore it.
+        if (!(event.getPlayer() instanceof ServerPlayer serverPlayer)) {
+            return;
+        }
+        // If it is not a Lumberjack Block we ignore it
+        if (!(event.getState().is(LumberjackBlocks.LUMBERJACK_OAK_LOG.get()))) {
+            return;
+        }
+        LumberjackAttachments.get(serverPlayer).addExperience(10L);
     }
 }
