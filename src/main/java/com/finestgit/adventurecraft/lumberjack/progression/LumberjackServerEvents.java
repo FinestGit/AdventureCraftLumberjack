bef084544e1bcd197bad0f91a3a83c3ef66aa8e4
Chase Rogness <chaserogness@gmail.com>
2026-05-26 10:43:12 -0500

    fix: Using wrong event

--- src/main/java/com/finestgit/adventurecraft/lumberjack/progression/LumberjackServerEvents.java
+++ src/main/java/com/finestgit/adventurecraft/lumberjack/progression/LumberjackServerEvents.java
@@ -2,21 +2,18 @@ package com.finestgit.adventurecraft.lumberjack.progression;
 
 import com.finestgit.adventurecraft.lumberjack.AdventureCraftLumberjack;
 
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.server.ServerStartedEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 
 @EventBusSubscriber(modid = AdventureCraftLumberjack.MODID)
 public class LumberjackServerEvents {
     @SubscribeEvent
-    public static void onServerStarting(ServerStartedEvent event) {
-        AdventureCraftLumberjack.LOGGER.info("Server started; players online: {}",
-                event.getServer().getPlayerList().getPlayerCount());
-        for (ServerPlayer player : event.getServer().getPlayerList().getPlayers()) {
-            LumberjackData data = LumberjackAttachments.get(player);
-            AdventureCraftLumberjack.LOGGER.info("Player Experience: {}", data.getTotalExperience());
-            AdventureCraftLumberjack.LOGGER.info("Player Level: {}", data.getLevel());
-        }
+    public static void onPlayerJoined(PlayerLoggedInEvent event) {
+        Player player = event.getEntity();
+        LumberjackData data = LumberjackAttachments.get(player);
+        AdventureCraftLumberjack.LOGGER.info("Player Experience: {}", data.getTotalExperience());
+        AdventureCraftLumberjack.LOGGER.info("Player Level: {}", data.getLevel());
     }
 }
