03edc4b8ff77b260e2b6e8d1c33470f5f55870db
Chase Rogness <chaserogness@gmail.com>
2026-05-27 10:16:24 -0500

    fix: registerBlock instead of register to try and use custom block

--- src/main/java/com/finestgit/adventurecraft/lumberjack/registry/LumberjackBlocks.java
+++ src/main/java/com/finestgit/adventurecraft/lumberjack/registry/LumberjackBlocks.java
@@ -13,13 +13,12 @@ import net.neoforged.neoforge.registries.DeferredItem;
 public class LumberjackBlocks {
         // Block Registration
         public static final DeferredBlock<LumberjackLogBlock> LUMBERJACK_OAK_LOG = ModRegistry.BLOCKS
-                        .register("lumberjack_oak_log",
-                                        () -> new LumberjackLogBlock(
-                                                        BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG)
-                                                                        .mapColor(MapColor.WOOD)
-                                                                        .sound(SoundType.WOOD),
-                                                        1, 10,
-                                                        true));
+                        .registerBlock(
+                                        "lumberjack_oak_log",
+                                        properties -> new LumberjackLogBlock(properties, 1, 10, true),
+                                        () -> BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG)
+                                                        .mapColor(MapColor.WOOD)
+                                                        .sound(SoundType.WOOD));
 
         // Block Item Registration
         public static final DeferredItem<BlockItem> LUMBERJACK_OAK_LOG_ITEM = ModRegistry.ITEMS
